6b879994153727e3f28e787175db2c3e01c65739
sudhirerahul <sudhirerahul.karun@gmail.com>
2026-01-20 13:04:13 -0500

    upload_sql_files

---
+++ sql_load/1_top_paying_jobs.sql
@@ -0,0 +1,18 @@
+SELECT
+    job_id,
+    job_title_short,
+    job_schedule_type,
+    job_location,
+    salary_year_avg,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN
+    company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_location = 'Anywhere' AND
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
